12b907744b16e73d6f601b3d82108c8406756ad6
leoff12 <165836552+leoff12@users.noreply.github.com>
2024-05-24 10:40:29 -0600

    sesion23424

---
+++ sesion23424/ejercicio1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void esPar(int num);
+
+main()
+{
+
+    int num;
+    cout << "Introduce un numero";
+    cin >> num;
+    esPar (num);
+}
+
+void esPar(int num);
+{
+    if (num % 2 == 0){
+        printf
+    }
+}
